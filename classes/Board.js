630ac685a2544f82d69c7667c69884c635f0d4ac
Isabelle <isabelle.xu@wustl.edu>
2018-08-29 14:59:17 -0500

    update

--- classes/Board.js
+++ classes/Board.js
@@ -5,8 +5,21 @@
 
 class Board {
 
-  constructor() {
-    
+  constructor(tiles) {
+    this.tiles = tiles; // holds array of tiles
+  }
+
+  /*
+  * Shuffles the contents of an array
+  * @param array e
+  * referenced from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+  */
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; --i) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return e;
   }
 
 }
--- classes/Game.js
+++ classes/Game.js
@@ -6,21 +6,15 @@
 class Game {
 
 	constructor(tiles) {
-		this.tiles = tiles;
 		this.score = 0;
 	}
 
-  	/*
- 	* Shuffles the contents of an array
- 	* @param array e
- 	* referenced from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
- 	*/
- 	function shuffle(array) {
- 		for (let i = array.length - 1; i > 0; --i) {
- 			const j = Math.floor(Math.random() * (i + 1));
- 			[array[i], array[j]] = [array[j], array[i]];
- 		}
- 		return e;
- 	}
+	function newGame() {
+
+	}
+
+	function gameCompleted() {
+
+	}
 
 }
