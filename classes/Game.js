9c825a3f5a2fd09ba2c2690c2d1af5d61fa180d8
Isabelle <isabelle.xu@wustl.edu>
2018-08-29 15:19:07 -0500

    update

--- classes/Game.js
+++ classes/Game.js
@@ -5,12 +5,23 @@
 
 class Game {
 
-	constructor(tiles) {
+	constructor() {
 		this.score = 0;
 	}
 
+	/**
+	* upon opening game user is prompted if they want to load a previously saved
+	* game or if they want to have a new board.
+	*/
 	function newGame() {
+		 if(confirm("Do you wish to load a saved game?")) {
+			 // check to see if a saved game exists
 
+		 } else {
+			 alert("Starting a new game");
+			 this.score = 0;
+			 document.getElementbyId("score").innerHTML = 0;
+		 }
 	}
 
 	function gameCompleted() {
