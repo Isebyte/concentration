9dfc9d199e3365131044f8a93a6617c5dfec26b7
Jeremy Goldstein <jeremy.goldstein@wustl.edu>
2018-08-29 15:14:50 -0500

    initial

--- classes/Tile.js
+++ classes/Tile.js
@@ -5,8 +5,26 @@
 
 class Tile {
 
-  constructor() {
-    
+  constructor(x_coordinate, y_coordinate, logo) {
+    this.answerVisible = false
+    this.isMatched = false
+    this.x_coordinate = x_coordinate
+    this.y_coordinate = y_coordinate
+    this.logo = logo
+    this.addEventListener("click", flip);
   }
 
+  flip(){
+        //TODO: animation in JQuery
+  }
+}
+
+checkMatch(a, b){
+  if(a.logo == b.logo){
+    a.isMatched = true
+    b.isMatched = true
+
+    a.answerVisible = true
+    b.answerVisible = true
+  }
 }
