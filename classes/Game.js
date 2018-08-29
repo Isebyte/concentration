f2d1f8d16fce80e008125817a42f4f082de6798a
Isabelle <isabelle.xu@wustl.edu>
2018-08-28 22:20:51 -0500

    added css file

--- classes/Game.js
+++ classes/Game.js
@@ -1,1 +1,13 @@
-
+/*
+ * Shuffles the contents of an array
+ * @param array e
+ * @return array e
+ * referenced from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ */
+function shuffle(e) {
+    for (let i = e.length - 1; i > 0; --i) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [e[i], e[j]] = [e[j], e[i]];
+    }
+    return e;
+}
